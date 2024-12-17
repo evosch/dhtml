9222c0bc61fa2a09670bfeaa3cea6a0db9b22d9c
Erwin <evosch@users.noreply.github.com>
2024-12-17 22:56:05 +0100

    Update monitor.js

--- worker/monitor.js
+++ worker/monitor.js
@@ -2,15 +2,18 @@ import { _has } from './helpers.js';
 import { PARSER } from './constants.js'
 
 class Monitor {
-  #id;
-  #prop;
+  #ref;
   #value;
   #deps;
 
-  constructor(id, prop, value, context) {
-    this.#id = id;
-    this.#prop = prop;
-    this.#value = value;
+  /**
+  * define a new monitor
+  * @params {any} ref - a reference to the object requesting the monitor
+  * @params {any} value - the parsable that needs to be monitored
+  * @params {Context} context - the context to obtain access to registered parsers
+  */
+  constructor(ref, value, context) {
+    this.#ref = ref;
     this.#deps = context.get()[PARSER].getDeps(value);
   }
 
@@ -26,7 +29,7 @@ class Monitor {
   evaluate(context) {
     const value = context[PARSER].evaluate(this.#value, context);
 
-    return [this.#id, this.#prop, value];
+    return [this.#ref, value];
   }
 }
 

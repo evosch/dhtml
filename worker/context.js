e82aa0b41072636a815f14b4865d6530bf0bd3e3
Erwin <evosch@users.noreply.github.com>
2024-12-17 22:41:44 +0100

    Update context.js

--- worker/context.js
+++ worker/context.js
@@ -1,12 +1,12 @@
 import { _set, _inflate } from './helpers.js';
 import Monitor from '././monitor.js';
 
-const STORE = Symbol('reference for storage');
+const STORAGES_KEY = Symbol('reference for storage');
 
 class Context  {
   #state = {};
   #observed = new Set();
-  #monitors = new Set();
+  #monitors = [];
   
   constructor(initialState = {}){
     this.#state = initialState;
@@ -16,58 +16,64 @@ class Context  {
     return this.#state;
   }
   
+  /**
+  * Register a new store into the context
+  * @params {string} name - the name by which the store can be accessed
+  * @params {}
+  */
   register(name, attr) {
-    super.register(name, attr);
+    // read original store data
     this.#state[name] = attr.getStore();
-    this.subscribe(STORE, name, name);
   }
   
+  /**
+  * update the state in the context
+  * @params {string[]} path - path to be updated
+  * @params {unknown} update - new value
+  */
   update(path, update) {
-    _set(this.#state, path, update);
+    this.#state = _set(this.#state, path, update);
     this.#observed.add(path);
   }
   
+  /**
+  * subscribe to changes of a specific part of the context
+  * @params {string} id - ??
+  * @params {string[]} prop - ??
+  * @params {string} value - ??
+  */
   subscribe(id, prop, value) {
     const monitor = new Monitor(id, prop, value, this);
-    this.#monitors.add(monitor);
+    this.#monitors.push(monitor);
     return monitor;
   }
   
+  /**
+  * remove a previous subscription
+  * @params {Monitor} monitor - the result of the previous subscription
+  */
   unsubscribe(monitor) {
-    return this.#monitors.delete(monitor);
-  }
-
-  #store() {
-    if (!this.#state[STORE]) {
-      return;
-    }
-
-    Object.entries(this.#state[STORE]).forEach(([k, v]) => {
-      const { setStore } = this.#state[k];
-      setStore(this.#state);
-    });
+    this.#monitors = this.#monitors.filter(m => m !== monitor);
   }
   
+  /**
+  * take all changes since the last flush, check which subscriptions are affected and calculate a computed response if required
+  */
   async flush() {
     const toEvaluate = new Set();
     for (const monitor of this.#monitors) {
       if (monitor.affected(this.#observed)) {
-        toEvaluate.add(monitor);
+         changes.push(parser.evaluate(this.#state));
       }
     }
     
-    if (toEvaluate.size < 1) { return; }
+    if(changes.length <= 0) { return; }
+
+    const result = await Promise.all(changes);
     
-    const changes = [];
-    for (const parser of toEvaluate) {
-      changes.push(await parser.evaluate(this.#state));
-    }
+    self.postMessage(result);
     
-    const changeObject = _inflate(changes);
-    
-    self.postMessage(changeObject);
-    
-    this.#store();
+    // TODO update stores
   }
 }
 
